f47278412b35ea25691930ff859ab5a6df22db1d
msolehrois <soleh.rois62@gmail.com>
2020-08-02 20:02:05 +0700

    Upload Repository GitHub

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ahmad Candra W.W
+                   Muhammad Soleh Rois
                 </div>
 
                 <div class="links">
